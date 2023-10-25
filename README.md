9b1d97814f53ff2178b09fe0e59cbaad20dfcb22
Dmitry Alinsky <alinsky.dmitry@gmail.com>
2023-10-25 08:58:10 +0500

    Update

--- README.md
+++ README.md
@@ -41,14 +41,6 @@ npm install
 npm run dev
 ```
 
-### Runtime possible error fix
-1. If you'll get error in console, remove **type="module"** from **<script ...** tags in {your_project_folder}/vue/index_prod.html.
-2. Rerun
-```
-npm run dev
-```
-3. Revert all changes in {your_project_folder}/vue/index_prod.html
-
 ## Folders structure
 
 Main folders you need:
--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,11 +1,25 @@
 import { fileURLToPath, URL } from 'url';
+import fs from 'fs/promises';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        {
+            name: 'build-html',
+            apply: 'build',
+            transformIndexHtml: {
+                enforce: 'pre',
+                async transform() {
+                    let html = await fs.readFile('./index.html', 'utf8');
+                    return html.replaceAll('<script src=', '<script type="module" src=');
+                },
+            },
+        },
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -13,8 +27,5 @@ export default defineConfig({
     },
     build: {
         assetsDir: 'particles',
-        rollupOptions: {
-            input: './index_prod.html',
-        },
     },
 });
