d3b00a648a0c526f98d7093422415e43372b3a90
Dmitry Alinsky <alinsky.dmitry@gmail.com>
2023-11-05 15:06:09 +0500

    Update cache.php

--- config/cache.php
+++ config/cache.php
@@ -52,6 +52,7 @@ return [
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
+            'permission' => 0777,
         ],
 
         'memcached' => [
